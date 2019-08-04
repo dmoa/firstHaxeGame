fbed7991277168a57b22d220c5ccc15f01e467a0
Stan <44374106+dmoa@users.noreply.github.com>
2019-08-04 20:39:57 +0300

    basic setup

---
+++ Source/Main.hx
@@ -0,0 +1,19 @@
+package;
+
+import openfl.display.Sprite;
+import openfl.events.Event;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+
+
+	}
+
+	private function eventResponse (KeyboardEvent):Void 
+	{
+
+	}
+}
